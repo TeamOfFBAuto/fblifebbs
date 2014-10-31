3f0be97338799c608a5f6c06a4a820a2f6d25062
fblife-zhang <zhangshaonan@fblife.com>
2014-10-31 19:27:16 +0800

    事实上事实上
    收拾收拾

--- fblifebbs/Controls/StartControl/Views/RankingListSegmentView.m
+++ fblifebbs/Controls/StartControl/Views/RankingListSegmentView.m
@@ -16,33 +16,35 @@
     if (self)
     {
         rankingListBlock = theBlock;
-        
-        NSArray * image_array = [NSArray arrayWithObjects:@"bbs_rankinglist_zhuti1",@"bbs_rankinglist_chexing1",@"bbs_rankinglist_dadui1",@"bbs_rankinglist_zhuti",@"bbs_rankinglist_chexing",@"bbs_rankinglist_dadui",nil];
-        
-        for (int i = 0;i < 3;i++)
-        {
-            UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
-            button.frame = CGRectMake(12 + 100*i,12,96,45);
-            [button setImage:[UIImage imageNamed:[image_array objectAtIndex:i]] forState:UIControlStateNormal];
-            [button setImage:[UIImage imageNamed:[image_array objectAtIndex:i+3]] forState:UIControlStateSelected];
-            historyPage = 0;
-            
-            if (i == 0)
-            {
-                button.selected = YES;
-                [button setImageEdgeInsets:UIEdgeInsetsZero];
-            }else
-            {
-                button.selected = NO;
-                [button setImageEdgeInsets:UIEdgeInsetsMake(0,0,5,0)];
-            }
-            
-            button.tag = 100+i;
-            [button addTarget:self action:@selector(buttonTap:) forControlEvents:UIControlEventTouchUpInside];
-            [self addSubview:button];
-        }
+        [self setup];
     }
     return self;
+}
+
+-(void)setup
+{
+    NSArray * image_array = [NSArray arrayWithObjects:@"bbs_rankinglist_zhuti1",@"bbs_rankinglist_chexing1",@"bbs_rankinglist_dadui1",@"bbs_rankinglist_zhuti",@"bbs_rankinglist_chexing",@"bbs_rankinglist_dadui",nil];
+    
+    for (int i = 0;i < 3;i++)
+    {
+        UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake(12 + 100*i,12,96,45);
+        [button setImage:[UIImage imageNamed:[image_array objectAtIndex:i]] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:[image_array objectAtIndex:i+3]] forState:UIControlStateSelected];
+        historyPage = 0;
+        
+        button.tag = 100+i;
+        [button addTarget:self action:@selector(buttonTap:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:button];
+        
+        if (i == 0)
+        {
+            button.selected = YES;
+        }else
+        {
+            button.selected = NO;
+        }
+    }
 }
 
 
@@ -56,9 +58,7 @@
     {
         UIButton * button = (UIButton *)[self viewWithTag:historyPage+100];
         button.selected = NO;
-        [button setImageEdgeInsets:UIEdgeInsetsMake(0,0,5,0)];
         sender.selected = YES;
-        [sender setImageEdgeInsets:UIEdgeInsetsZero];
     }
     historyPage = sender.tag -100;
 }
